2909d972db4db555984cab3bbf7168d29d7b393f
James Jordan <jpjordan1987@gmail.com>
2021-02-21 17:18:55 -0500

    first jQuery command entered, folder structure improved

---
+++ Assets/javascript/script.js
@@ -0,0 +1,51 @@
+// GIVEN I am using a daily planner to create a schedule
+
+// WHEN I open the planner
+// THEN the current day is displayed at the top of the calendar
+
+var dayOfWeek = $('#currentDay').text=moment().format('dd');
+
+
+// WHEN I scroll down
+// THEN I am presented with timeblocks for standard business hours
+
+// for (var i = 0; i < 1; i++) {  //CHANGE MAX
+//     // Create button
+//     var letterBtn = $('<button>');
+//     // Assign style to the button
+//     letterBtn.addClass('letter-button btn btn-info');
+//     // Assign the letter to the data-letter attribute
+//     letterBtn.attr('data-letter', letters[i]);
+//     // Display the letter
+//     letterBtn.text(letters[i]);
+//     // Attach the letter element
+//     buttonListEl.append(letterBtn);
+//   }
+// }
+
+
+// WHEN I view the timeblocks for that day
+// THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+
+
+// WHEN I click into a timeblock
+// THEN I can enter an event
+
+// WHEN I click the save button for that timeblock
+// THEN the text for that event is saved in local storage
+
+// WHEN I refresh the page
+// THEN the saved events persist
+
+
+
+// console.log(moment("13:00", 'HH:mm').format('hh:mm a'));
+
+
+
+
+
+
+// Dynamically create buttons
+  // Create a for-loop to iterate through the timeblocks.
+
