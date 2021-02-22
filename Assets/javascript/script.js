9d0a96f312431d1a92a011f4aff487f8c7609173
James Jordan <jpjordan1987@gmail.com>
2021-02-22 18:18:55 -0500

    have the timeblock format figured out, have some of the time appending jquery figured out

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,37 +1,57 @@
+    // works for hour, am/pm  ***************RESOURCE**************************
+    //$('#currentDay').text(moment().format('hh a'));
 
+// ***************BEGIN**************************
 // GIVEN I am using a daily planner to create a schedule
 
 // WHEN I open the planner
 // THEN the current day is displayed at the top of the calendar
-    var nowHR = moment();
-    $('#currentDay').text(moment().format('dddd'));
     
-    // =('Wednesday'); //moment().format('dd')
-
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
+    
 
 // WHEN I scroll down
 // THEN I am presented with timeblocks for standard business hours
 
 //stack overflow question 25498082
- var timeBlockHtml = '<div class="row">' +
-                    '<div class="description"></div>'+
-                    '<div class="hour"></div>'+
-                    '<div class="time-block" >'+
-                    '<textarea class="future"></textarea>'+
-                    '</div>'+
-                    '<div class="saveBtn"></div>'+
-                    '</div>'+
-                    '</div>';
+
 
 function buildWorkDay() {
-    for (i=8; i < 18; i++){
-        $(timeBlockHtml).appendTo('.container')
-        }
+    for (i=9 ; i < 19 ; i++){
+        
+        var timeBlockHtml = '<div class="row time-block" id="' + i + '">' +
+        '<div class="hour col-1 p-3">' + moment(i,'hour').format('hh a') + '</div>' +
+        '<textarea class="description" cols="100" rows="3"></textarea>' +
+        '<button class="btn saveBtn col"> <i class="fas fa-unlock-alt"> </i> </button>' +
+        '</div>';
+        $(timeBlockHtml).appendTo('.container');
+        
+        // '<div class="description"></div>' +
+        // '<div class="hour">' + moment(i.toString()).format('hh a')  + '</div>' +
+        // '<div class="time-block" >'+
+        // '<textarea class="future"></textarea>'+
+        // '</div>'+
+        // '<div class="saveBtn"></div>'+
+        // '</div>'+
+        // '</div>';
+    }
 }                    
 
-
-
 buildWorkDay();
+
+// var today = new Date();
+// var now;
+
+// var test=moment(8);
+// console.log(test);
+
+// now=moment(today)
+
+// moment().format('hA');
+
+// console.log((now).format('hh a'));
+// console.log((now).format('hh a'));
+
 
 //            '<div class="parent">' + 
 //                '<div class="loadimg"></div>' +
