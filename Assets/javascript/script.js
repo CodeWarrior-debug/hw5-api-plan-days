a086bea9a6f0c201dc872f8d2a300392d75c8aea
James Jordan <jpjordan1987@gmail.com>
2021-02-23 22:52:00 -0500

    final code workinggit add index.html git add index.html  pushing now and will send up readme soon

--- Assets/javascript/script.js
+++ Assets/javascript/script.js
@@ -1,71 +1,37 @@
-// GIVEN I am using a daily planner to create a schedule // WHEN I open the planner// THEN the current day is displayed at the top of the calendar
-    var buttonListEl = $('#buttons');
-    var savedNotes;
-    var newNotes={};
-    // var t9={}; var t10={}; var t11={}; var t12={}; var t13={}; var t14={}; var t15={}; var t16={}; var t17={}; var t18={};
-
-    //var timeArray=[t9, t10, t11, t12, t13, t14, t15, t16, t17, t18]
-    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY'));
+    $('#currentDay').text(moment().format('dddd, MMMM Do YYYY')); //display current day at top
     
 // THEN  timeblocks for standard business hours//THEN each timeblock is color coded to indicate whether it is in the past, present, or future//// WHEN I click into a timeblockI can enter an event
 //stack overflow question 25498082
 
 function buildWorkDay() {
-    for (i=9 ; i < 19 ; i++){
-        
+    for (i=9 ; i < 18 ; i++){ 
         var timeTense;
+        var timeString =('#text' + i).toString();
         var timeHrInterval = parseInt(moment(i,'hour').format('H'));
         var hourNow=parseInt(moment().format('H'));
 
-            //Check if workday hour is past,present,future
-
-        if (timeHrInterval < hourNow){
+        if (timeHrInterval < hourNow){  //Check if workday hour is past,present,future
             timeTense='past';
         } else if (timeHrInterval===hourNow) { 
             timeTense='present';
         } else {timeTense='future'};
 
-            //Send timeblock html code to index.html with id's and classes being added dynamically
-
-        var timeBlockHtml = '<div class="row time-block" id="' + i + '">' +
+        var timeBlockHtml = '<div class="row time-block" id="' + i + '">' + //design timeblock html code to index.html dynamically
         '<div class="hour col-1 p-3">' + moment(i,'hour').format('h A') + '</div>' +
-        '<textarea class="description txt '+ timeTense +'" group="' + i + '"cols="100" rows="3"></textarea>' +
+        '<textarea class="description txt '+ timeTense +'" id="text' + i + '"cols="100" rows="3">' + (JSON.parse(localStorage.getItem('text'+ i)) || "") + '</textarea>' +
         '<button class="btn saveBtn col" group="' + i + '"> <i class="fas fa-unlock-alt"> </i> </button>' +
         '</div>';
-
-            //add to page within container
-        $(timeBlockHtml).appendTo('.container');
+        $(timeBlockHtml).appendTo('.container'); //add to page within container
+        // $(timeString).value='halleluha'; //JSON.parse(localStorage.getItem('text'+ i));
     }
 }                    
-    //build HTML
 buildWorkDay();
-    //add listeners via delegation
-var buttonListEl = $('.saveBtn');
-    
-$('.saveBtn').click(function(){
-    
+
+    var buttonListEl = $('.saveBtn'); //add listeners via delegation
+    $('.saveBtn').click(function(){
     var thisBtn=$(this);
-    var btnNumber = thisBtn.attr('group');
+    var saveTag=( 'text' + thisBtn.attr('group')); //develop storage object tag
     var textareaOrdered= thisBtn.parent().children(1);
     var textareaText=textareaOrdered[1].value;
-    // localStorage.setItem['savedNotes'];
-    recordNotes();
-    function recordNotes(){
-        myNotes = {
-            text: [textareaText],
-            hour: [btnNumber],
-        }
-        savedNotes();
-                    }
-        function savedNotes() {
-        savedNotes = JSON.parse(localStorage.getItem("savedNotes")) || [];
-        if (savedNotes.length===0) {
-            localStorage.setItem("savedNotes", JSON.stringify(myNotes));
-        } else 
-        {
-            savedNotes.text.push(myNotes.text[0]);
-            savedNotes.hour.push(myNotes.hour[0]);
-            localStorage.setItem("savedNotes", JSON.stringify(savedNotes));
-        };
-                            }
-})
+    localStorage.setItem(saveTag, JSON.stringify(textareaText));
+    });
